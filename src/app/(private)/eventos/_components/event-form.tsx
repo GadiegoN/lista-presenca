057df190be3c1d1367add7a6f61f30b6e5b26a0c
GadiegoN <ngadiego@gmail.com>
2025-10-28 21:13:41 -0300

    feat: add placeholder time

--- src/app/(private)/eventos/_components/event-form.tsx
+++ src/app/(private)/eventos/_components/event-form.tsx
@@ -22,6 +22,7 @@ export function EventForm({ name, time, setName, setTime, onAdd }: Props) {
         className="border rounded-lg px-3 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full sm:w-auto"
         type="time"
         value={time}
+        placeholder="Horario do evento"
         onChange={(e) => setTime(e.target.value)}
       />
 
